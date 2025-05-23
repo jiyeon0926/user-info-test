426ae07447ced98c92e2c7f03bd76c5201ed92cd
jiyeon0926 <yeoni9094@naver.com>
2025-05-23 14:00:57 +0900

    feat: DTO

---
+++ src/main/java/com/example/demo/domain/user/dto/TokenDto.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain.user.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TokenDto {
+
+    private final String token;
+}
